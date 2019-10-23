bda6f2dad44b24c5db0dc681a33e10399727dc97
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-10-23 09:49:28 -0300

    Writing Test 04 - Create attendee with automatic fields preset

--- tests/test_service_attendant.py
+++ tests/test_service_attendant.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 from services.service_attendant import AttendeeService
 from errors.exceptions import MedievalBankException
 from persistence.models import Attendee
@@ -17,9 +18,9 @@ class TestAttendeeService(unittest.TestCase):
         self.assertIsNotNone(created_attendee.creation_date)
         self.assertEquals(name, created_attendee.name)
 
-    def try_create_attendee_with_error(self, name, fail_message, expected_exception_message):
+    def try_create_attendee_with_error(self, attendee, fail_message, expected_exception_message):
         try:
-            self.create_attendee(name)
+            self.service.create(attendee)
             self.fail(fail_message)
         except MedievalBankException as e:
             self.assertEquals(expected_exception_message, e.message)
@@ -36,14 +37,33 @@ class TestAttendeeService(unittest.TestCase):
     def t02_create_attendee_without_name(self):
         fail_message = "Test failed because the system accepted to create an attendee without name"
         expected_exception_message = "Name is mandatory"
-        self.try_create_attendee_with_error(None, fail_message, expected_exception_message)
+        attendee = Attendee()
+        self.try_create_attendee_with_error(attendee, fail_message, expected_exception_message)
 
     def t03_attendee_name_duplicated(self):
         a_name = "A Name"
         fail_message = "Test failed because the system accepted to create an attendee with an already existent name"
         expected_exception_message = "Attendee name cannot be duplicated"
         self.create_attendee(a_name)
-        self.try_create_attendee_with_error(a_name, fail_message, expected_exception_message)
+        attendee2 = self.create_attendee(a_name)
+        self.try_create_attendee_with_error(attendee2, fail_message, expected_exception_message)
+
+    def t04_create_attendee_with_automatic_fields(self):
+        a_name = "A Name"
+
+        attendee = Attendee()
+        attendee.name = a_name
+        attendee.id = 123
+        fail_message = "Test failed because the system accepted to create attendee with id already set"
+        expected_exception_message = "Attendee id cannot be set"
+        self.try_create_attendee_with_error(attendee, fail_message, expected_exception_message)
+
+        attendee2 = Attendee()
+        attendee2.name = a_name
+        attendee2.creation_date = datetime.now()
+        fail_message = "Test failed because the system accepted to create attendee with creation date already set"
+        expected_exception_message = "Attendee creation date cannot be set"
+        self.try_create_attendee_with_error(attendee2, fail_message, expected_exception_message)
 
 
 if __name__ == '__main__':

49e9460ac680dc55446cab4383bfa433c5ccc1d3
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-10-23 09:40:05 -0300

    Refactoring tests

--- tests/test_service_attendant.py
+++ tests/test_service_attendant.py
@@ -4,43 +4,46 @@ from errors.exceptions import MedievalBankException
 from persistence.models import Attendee
 
 
-service = AttendeeService()
-
-
 class TestAttendeeService(unittest.TestCase):
+    service = AttendeeService()
 
-    def t01_create_attendee(self):
+    def create_attendee(self, name):
         attendee = Attendee()
-        attendee.name = "Layla"
-        created_attendee = service.create(attendee)
+        attendee.name = name
+        return self.service.create(attendee)
 
+    def validate_attendee_creation(self,name, created_attendee):
         self.assertIsNotNone(created_attendee.id)
         self.assertIsNotNone(created_attendee.creation_date)
-        self.assertEquals(attendee.name, created_attendee.name)
+        self.assertEquals(name, created_attendee.name)
 
-        searched_attendee = service.get_one(created_attendee.id)
+    def try_create_attendee_with_error(self, name, fail_message, expected_exception_message):
+        try:
+            self.create_attendee(name)
+            self.fail(fail_message)
+        except MedievalBankException as e:
+            self.assertEquals(expected_exception_message, e.message)
+
+    def t01_create_attendee(self):
+        a_name = "A Name"
+        created_attendee = self.create_attendee(a_name)
+
+        self.validate_attendee_creation(created_attendee)
+
+        searched_attendee = self.service.get_one(created_attendee.id)
         self.assertEquals(created_attendee, searched_attendee)
 
     def t02_create_attendee_without_name(self):
-        attendee = Attendee()
-        try:
-            service.create(attendee)
-            self.fail("Accepted attendee without name")
-        except MedievalBankException as e:
-            self.assertEquals("Name is mandatory", e.message)
+        fail_message = "Test failed because the system accepted to create an attendee without name"
+        expected_exception_message = "Name is mandatory"
+        self.try_create_attendee_with_error(None, fail_message, expected_exception_message)
 
     def t03_attendee_name_duplicated(self):
-        attendee = Attendee()
-        attendee.name = "Ana"
-        service.create(attendee)
-
-        aux_attendee = Attendee()
-        aux_attendee.name = "Ana"
-        try:
-            service.create(aux_attendee)
-            self.fail("Created attendee with existent name")
-        except MedievalBankException as e:
-            self.assertEquals("Name already exists", e.message)
+        a_name = "A Name"
+        fail_message = "Test failed because the system accepted to create an attendee with an already existent name"
+        expected_exception_message = "Attendee name cannot be duplicated"
+        self.create_attendee(a_name)
+        self.try_create_attendee_with_error(a_name, fail_message, expected_exception_message)
 
 
 if __name__ == '__main__':

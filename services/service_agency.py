2c771a8f2b9268915f787fa7554edc0cc1368e63
FranciscoUrsulino <francisco.ursulino@dcx.ufpb.br>
2019-12-03 12:04:37 -0300

    t014, falta terminar

---
+++ services/service_agency.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from persistence.models import Attendee, engine
+from sqlalchemy.orm import sessionmaker
+from errors.exceptions import MedievalBankException
+from errors.messages import AttendeeMessages
+import re
+
+Session = sessionmaker(bind=engine)
+
+
+# refactor do helper, extract method nas validações do update e create
+
+
+class AgencyService:
+    _session = None
+    _attendee_service = None
+    name = None
+    manager = None
+
+    def open_session(self):
+        self._session = Session()
+
+    def close_session(self):
+        self._session.close()
+
+    def __init__(self, attendee_service):
+        self._attendee_service = attendee_service
+
+    @staticmethod
+    def setName(name):
+        agency_service.name = name
+
+
+    @staticmethod
+    def setManager(manager):
+        agency_service.manager = manager
---
+++ tests/test_service_agency.py
@@ -0,0 +1,53 @@
+import unittest
+
+from service_agency import AgencyService
+
+import unittest
+from datetime import datetime
+from services.service_attendant import AttendeeService
+from .helpers import TestAttendeeServiceHelper as helper
+from persistence.models import Attendee, Base, tear_down_test_db
+from errors.messages import AttendeeMessages
+from time import sleep
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from persistence import test_db_path
+
+class TestServiceAgency(unittest.TestCase):
+
+    service_atendee = AttendeeService()
+    service_agency = AgencyService(service_atendee)
+
+    EX_NAME = "A Name"
+    EX_OTHER_NAME = "Other Name"
+    EX_EMAIL = "asd@gmail.com"
+    EX_SSN = "473-20-6799"
+    UNKNOWN_ID = 202020
+
+
+    def setUp(self):
+        self.service_atendee.open_session()
+
+    def tearDown(self):
+        tear_down_test_db()
+        self.service_atendee.close_session()
+
+    def test01_agency_status(self):
+        result = service_agency.getStatus() # Criar meth getStatus
+        # CRIAR assertEqual 
+
+    def test_initialConfig(self):
+        service_agency.setName("Burgosland")
+        self.assertEqual("Burgosland", service_agency.getName())
+        service_agency.setManager("Joseph")
+        self.assertEqual("Joseph", service_agency.getManager())
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
+
+
